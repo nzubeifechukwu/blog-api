487ba51a60c272baf9f116146629ba740458acce
nzubeifechukwu <nzube.ifechukwu@gmail.com>
2026-05-02 17:02:44 +0100

    Add Session table

--- authenticators/authenticators.js
+++ authenticators/authenticators.js
@@ -2,23 +2,30 @@ const prisma = require("../lib/prisma");
 const LocalStrategy = require("passport-local").Strategy;
 const bcrypt = require("bcryptjs");
 
-const localStrategy = new LocalStrategy(async (username, password, done) => {
-  try {
-    const user = await prisma.user.findUnique({ where: { email: username } });
-    if (!user) {
-      return done(null, false, { message: "Incorrect email and/or password." });
-    }
+const localStrategy = new LocalStrategy(
+  { usernameField: "email", passwordField: "password" },
+  async (email, password, done) => {
+    try {
+      const user = await prisma.user.findUnique({ where: { email: email } });
+      if (!user) {
+        return done(null, false, {
+          message: "Incorrect email and/or password.",
+        });
+      }
 
-    const match = await bcrypt.compare(password, user.password);
-    if (!match) {
-      return done(null, false, { message: "Incorrect email and/or password." });
-    }
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        return done(null, false, {
+          message: "Incorrect email and/or password.",
+        });
+      }
 
-    return done(null, user);
-  } catch (error) {
-    return done(error);
-  }
-});
+      return done(null, user);
+    } catch (error) {
+      return done(error);
+    }
+  },
+);
 
 function serializeSession(user, done) {
   done(null, user.id);
