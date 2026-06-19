9905b0d6ceadf310fe39fd09f7870a080714c5a6
nzubeifechukwu <nzube.ifechukwu@gmail.com>
2026-06-19 05:35:19 +0100

    Write validators for new user, post, comment and id param

--- routes/router.js
+++ routes/router.js
@@ -3,16 +3,22 @@ const passport = require("passport");
 
 const controllers = require("../controllers/controllers");
 const { requireAuthor, optionalAuth } = require("../middleware/auth");
+const { idParamValidator, signUpValidator, postValidator, commentValidator } = require("../middleware/validators");
 
 const router = Router();
 
 // Public routes
 router.get("/", controllers.home);
 router.get("/posts", controllers.getPublishedPosts);
-router.get("/posts/:id", optionalAuth, controllers.getPostById); // uses optionalAuth so authors can see their unpublished drafts
+router.get(
+  "/posts/:id",
+  optionalAuth, // allows authors to see their unpublished drafts
+  idParamValidator,
+  controllers.getPostById,
+);
 
 // Authentication & Registration
-router.post("/users", controllers.createUser);
+router.post("/users", signUpValidator, controllers.createUser);
 router.post("/login", controllers.loginUser);
 
 // Protected routes
@@ -20,6 +26,7 @@ router.post(
   "/posts",
   passport.authenticate("jwt", { session: false }), // Authenticate the user (Verify token & set req.user)
   requireAuthor, // Check if the user has the AUTHOR role
+  postValidator,
   controllers.createPost,
 );
 router.patch(
@@ -30,21 +37,26 @@ router.patch(
 router.post(
   "/posts/:id/comments",
   passport.authenticate("jwt", { session: false }),
+  idParamValidator,
+  commentValidator,
   controllers.createComment,
 );
 router.patch(
   "/posts/:id",
   passport.authenticate("jwt", { session: false }),
+  idParamValidator,
   controllers.updatePost,
 );
 router.delete(
   "/posts/:id",
   passport.authenticate("jwt", { session: false }),
+  idParamValidator,
   controllers.deletePost,
 );
 router.delete(
   "/comments/:id",
   passport.authenticate("jwt", { session: false }),
+  idParamValidator,
   controllers.deleteComment,
 );
 
